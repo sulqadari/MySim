40da7111561a1a3f9508e427336c96aa21f4eeb7
Telman <iti@atlas.card>
2021-07-05 18:06:59 +0300

    [base_classes-01] Preliminary PIN-based security system.

--- src/mysim/javacard/MySimApplet.java
+++ src/mysim/javacard/MySimApplet.java
@@ -5,45 +5,48 @@ import javacard.framework.ISOException;
 import javacard.framework.JCSystem;
 
 /**
- * Main class which implements functionality of the <code>javacard.framework.Applet</code>.<br>
+ * Main class which implements functionality of the <code>javacard.framework.MySimApplet</code>.<br>
  */
-public class Applet extends javacard.framework.Applet
+public class MySimApplet extends javacard.framework.Applet
 {
     private static AppletController controller  = null;     //Contains main functionality
 
     /**
      * Main constructor.<br>
      * This constructor initializes the instance of the <code>AppletConstructor</code> class
-     * encapsulates main functionality and security features of the this Applet.
+     * encapsulates main functionality and security features of the this MySimApplet.
      * @param pinTryLimit   the upper bound of the PIN attempts limit. Up to 9 tries.
      * @param pinSize       the maximum size of the PIN. Can't exceed 8 bytes
      *
      */
-    private Applet(byte pinTryLimit, byte pinSize)
+    private MySimApplet(byte pinTryLimit, byte pinSize)
     {
+        if ((pinTryLimit > (byte)9) || (pinSize != (byte)8))
+        {
+            PINException.throwIt(PINException.ILLEGAL_VALUE);
+        }
         controller  = new AppletController(pinTryLimit, pinSize);
     }
 
     /**
      * This method installs and registers a new instance of applet. After successful applet registration
      * the <code>AppletController.updatePin()</code> method is invoked to set initial PIN value.<br>
-     * @param bArray    Must contain the following subsequent of data:<br>
-     *                  <code>[tag_C9, ta_C9Len, pinLim, pinLen, pinByte1, ... pinByteN]</code>
-     * @param bOffset   RFU
-     * @param bLength   RFU
+     * @param bArray
+     * @param bOffset
+     * @param bLength
      * @throws ISOException
      */
     public static void install(byte[] bArray, short bOffset, byte bLength) throws ISOException
     {
-        byte pinTryLimit    = bArray[2];
-        byte pinSize        = bArray[3];
-        short pinOffset     = (short)4;
+        byte pinTryLimit    = bArray[0];
+        byte pinLength      = bArray[1];
+        short pinOffset     = bArray[2];
 
-        new Applet(pinTryLimit, pinSize).register();
+        new MySimApplet(pinTryLimit, pinLength).register();
 
-        JCSystem.beginTransaction();
-        controller.updatePin(bArray, pinOffset, pinSize);
-        JCSystem.commitTransaction();
+        changeAppletLifePhase();                            //Change applet life phase to INIT
+        controller.updatePin(bArray, pinOffset, pinLength);
+        changeAppletLifePhase();                            //Change applet life phase to USE
     }
 
     @Override
@@ -62,5 +65,10 @@ public class Applet extends javacard.framework.Applet
     {
        controller.resetPin();
        return true;
+    }
+
+    public void deselect()
+    {
+        controller.resetPin();
     }
 }
