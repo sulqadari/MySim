9ef5cc7e0bbc58a633f953df50fe546ab6c3c819
Telman <iti@atlas.card>
2021-07-06 14:56:18 +0300

    [aes_security-01] AES base objects. TODO: encryption/decryption methods.

--- src/mysim/javacard/AESController.java
+++ src/mysim/javacard/AESController.java
@@ -1,5 +1,9 @@
 package mysim.javacard;
 
+import javacard.security.AESKey;
+import javacard.security.KeyBuilder;
+import javacardx.crypto.Cipher;
+
 public class AESController
 {
     final static byte P1_INIT_KEY           = (byte)0x01;
@@ -9,5 +13,27 @@ public class AESController
     final static byte P2_DECRYPT            = (byte)0x00;
     final static byte P2_ENCRYPT            = (byte)0x01;
     
+    private Cipher cipher;
+    private AESKey key;
     
+    protected AESController()
+    {
+    	cipher	= Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+    	key		= (AESKey)KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+    }
+    
+    protected void initAesKey(byte mode)
+    {
+    	cipher.init(key, mode);
+    }
+    
+    protected void encryptData(byte[] array)
+    {
+    	
+    }
+    
+    protected void decryptData(byte[] array)
+    {
+    	
+    }
 }
